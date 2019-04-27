84e4ecbfafe74acec6dc3c5cf52cb340070a5ee0
peanutpowah <samuel.zelenka@hotmail.com>
2019-04-27 13:45:29 +0200

    Submit scene Level0

--- Fps399Protoype/Assets/Script/PlayerMovement.cs
+++ Fps399Protoype/Assets/Script/PlayerMovement.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     int movementSpeed;
+    int sprintSpeed;
 
     // Start is called before the first frame update
     void Start()
