d75824b603fd6a4c50fc9b226741d25e0ad09340
peanutpowah <samuel.zelenka@hotmail.com>
2019-05-02 23:27:30 +0200

    Room Transit Script added
    Script to switch rooms in real time

---
+++ Fps399Protoype/Assets/Script/RoomTransition.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomTransition : MonoBehaviour
+{
+
+    [SerializeField] List <GameObject> roomOnline;
+    [SerializeField] List <GameObject> roomOffline;
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            for (int i = 0; i < roomOnline.Count; i++)
+            {
+                roomOnline[i].SetActive(true);
+            }
+            for (int i = 0; i < roomOffline.Count; i++)
+            {
+                roomOffline[i].SetActive(false);
+            }
+        }
+    }
+
+}
