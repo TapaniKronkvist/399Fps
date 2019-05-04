28811c13f9af427fc50ab976501a2ca252240a6b
TapaniKronkvist <45991745+TapaniKronkvist@users.noreply.github.com>
2019-05-04 23:44:57 +0200

    TriggerOnEnter listener Ready.
    Class can call blockade to be disabled

---
+++ Fps399Protoype/Assets/ScritpsTappe/Blockade.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Blockade : MonoBehaviour
+{
+   public void Disable()
+    {
+        enabled = false;
+    }
+}
---
+++ Fps399Protoype/Assets/ScritpsTappe/TriggerOnEnter.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*This class is attached to a object that reakts to the player. This uses a listener and 
+activates method on the class being called. Set method in inspector. This class i 
+modular and works on any non static object. Written by Tapani Kronkvist*/
+
+public class TriggerOnEnter : MonoBehaviour
+{
+    public UnityEngine.Events.UnityEvent RemoveBlockade; //Disable blockade call;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "player")
+        {
+            RemoveBlockade.Invoke();
+        }
+    } //RemoveBlockade object is called and disabled via Blockade.Disable;
+
+}
